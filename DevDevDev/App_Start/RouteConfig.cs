285c3c15545cdb7d7e8f92e834055ed90521397a
Neil Campbell <neil@codecise.com>
2016-05-29 11:27:36 +1000

    Adding a 404 handler

--- DevDevDev/App_Start/RouteConfig.cs
+++ DevDevDev/App_Start/RouteConfig.cs
@@ -14,52 +14,64 @@ namespace DevDevDev
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
-              name: "submit-a-session",
-              url: "submit-a-session",
-              defaults: new { controller = "Main", action = "Submit", id = UrlParameter.Optional }
-          );
+                name: "submit-a-session",
+                url: "submit-a-session",
+                defaults: new { controller = "Main", action = "Submit", id = UrlParameter.Optional }
+            );
 
             routes.MapRoute(
-             name: "vote",
-             url: "vote",
-             defaults: new { controller = "Voting", action = "SessionsToVoteFor", id = UrlParameter.Optional }
-         );
+                name: "vote",
+                url: "vote",
+                defaults: new { controller = "Voting", action = "SessionsToVoteFor", id = UrlParameter.Optional }
+            );
 
             routes.MapRoute(
-             name: "view-sessions",
-             url: "view-sessions",
-             defaults: new { controller = "Main", action = "Sessions", id = UrlParameter.Optional }
-         );
+                name: "view-sessions",
+                url: "view-sessions",
+                defaults: new { controller = "Main", action = "Sessions", id = UrlParameter.Optional }
+            );
 
             routes.MapRoute(
-            name: "register",
-            url: "register",
-            defaults: new { controller = "Main", action = "register", id = UrlParameter.Optional }
-        );
+                name: "register",
+                url: "register",
+                defaults: new { controller = "Main", action = "register", id = UrlParameter.Optional }
+            );
 
             routes.MapRoute(
-            name: "agenda",
-            url: "agenda",
-            defaults: new {controller = "Main", action = "Agenda", id = UrlParameter.Optional}
-        );
-
-           routes.MapRoute(
-           name: "yow",
-           url: "yow",
-           defaults: new { controller = "Main", action = "yow", id = UrlParameter.Optional }
-       );
-
-           routes.MapRoute(
-            name: "code-of-conduct",
-            url: "code-of-conduct",
-            defaults: new { controller = "Main", action = "CodeOfConduct", id = UrlParameter.Optional }
-        );
+                name: "agenda",
+                url: "agenda",
+                defaults: new {controller = "Main", action = "Agenda", id = UrlParameter.Optional}
+            );
 
             routes.MapRoute(
-            name: "Default",
-            url: "{controller}/{action}/{id}",
-            defaults: new { controller = "Main", action = "Index", id = UrlParameter.Optional }
-        );
+               name: "yow",
+               url: "yow",
+               defaults: new { controller = "Main", action = "yow", id = UrlParameter.Optional }
+            );
+
+            routes.MapRoute(
+               name: "code-of-conduct",
+               url: "code-of-conduct",
+               defaults: new { controller = "Main", action = "CodeOfConduct", id = UrlParameter.Optional }
+            );
+
+            routes.MapRoute(
+               name: "not-found",
+               url: "not-found",
+               defaults: new { controller = "Error", action = "NotFound", id = UrlParameter.Optional }
+            );
+
+            routes.MapRoute(
+               name: "error",
+               url: "error",
+               defaults: new { controller = "Error", action = "Index", id = UrlParameter.Optional }
+            );
+
+            routes.MapRoute(
+               name: "Default",
+               url: "{controller}/{action}/{id}",
+               defaults: new { controller = "Main", action = "Index", id = UrlParameter.Optional }
+            );
         }
     }
 }
---
+++ DevDevDev/Controllers/ErrorController.cs
@@ -0,0 +1,17 @@
+﻿using System.Web.Mvc;
+
+namespace DevDevDev.Controllers
+{
+    public class ErrorController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View("Error");
+        }
+
+        public ActionResult NotFound()
+        {
+            return View("NotFound");
+        }
+    }
+}
